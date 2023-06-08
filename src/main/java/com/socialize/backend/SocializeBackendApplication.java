dfe5fbefa28fc7e8d230103e108c70900590a865
ZsomborBalatoni <zsombor.balatoni@gmail.com>
2023-06-09 00:42:00 +0200

    Uploading spring app

---
+++ src/main/java/com/socialize/backend/SocializeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.socialize.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocializeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocializeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialize/backend/SocializeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialize.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocializeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
